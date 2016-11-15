5fc6b50b85b2a2706edf258a2f27cb14980e87e6
saumya1510 <saumya1510@gmail.com>
2016-11-14 23:55:58 -0700

    Update imageScraper.R

--- imageScraper.R
+++ imageScraper.R
@@ -1,3 +1,6 @@
+#This program downloads images from a specifiec website. For reference, if run without making any changes
+#it would download the first 200 issues of the JL8 webcomics.
+#The code uses the following libraries.
 library(rvest)
 library(magrittr)
 
@@ -10,7 +13,7 @@ for (issue in 1:200){
   if (!file.exists(dest)){
     pgurl <- paste("http://www.limbero.org/", subDir, "/", issue, sep = "")
     pghtml <- read_html(pgurl)
-    nodes <- html_nodes(pghtml, xpath = "//*[@id=\"nav\"]/b[2]/img")
+    nodes <- html_nodes(pghtml, xpath = "//*[@id=\"nav\"]/b[2]/img")	#identifies the img tag in the source code of the site.
     imgsrc <- html_attr(x = nodes[[length(nodes)]], "src")
     message(paste("Downloading ", dest, "...", sep = ""))
     download.file(imgsrc, destfile = dest, mode = "wb", quiet = T)
